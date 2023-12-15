f652990ead419e73f1e3d5ff840e8d060e52407d
Boris Borisov <bborrisow@gmail.com>
2023-12-15 16:41:33 +0100

    add UI tests to CI/CD

--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-book');
+    await page.goto('https://borrisow-retake-exam.onrender.com/add-book');
     const form = await page.$('list');
     expect(form).toBeFalsy();
   });
